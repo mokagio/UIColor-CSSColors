7c6163d6081ca8694a85c9589fa377e718b2ce04
Gio <giovanni.lodi42@gmail.com>
2014-04-08 21:03:29 +0100

    New script to generate colors from W3C
    http://www.w3.org/TR/css3-color/#svg-color

---
+++ generator.rb
@@ -0,0 +1,36 @@
+
+colors_data = []
+
+header_template = <<TEMPLATE
++ (UIColor *)css@name@Color;
+
+TEMPLATE
+
+implementation_template = <<TEMPLATE
++ (UIColor *)css@name@Color
+{
+    return [UIColor colorWithRed:@red@ green:@green@ blue:@blue@ alpha:1.0];
+}
+
+TEMPLATE
+
+File.readlines("css_colors.txt").each do |line|
+  name = line.split(' ')[0]
+  values = line.split(' ')[-1].split(',')
+  red = values[0]
+  green = values[1]
+  blue = values[2]
+  colors_data.push( { :name => name, :red => red, :green => green, :blue => blue } )
+end
+
+colors_data.each do |color|
+  puts header_template.gsub '@name', color[:name]
+end
+
+colors_data.each do |color|
+  puts implementation_template
+    .gsub( '@name@', color[:name].capitalize )
+    .gsub( '@red@', "#{color[:red].to_f / 255.0}" )
+    .gsub( '@green@', "#{color[:green].to_f / 255.0}" )
+    .gsub( '@blue@', "#{color[:blue].to_f / 255.0}" )
+end
