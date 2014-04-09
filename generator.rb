b44438688730489cd0918ffd32ff26ff412fe265
Gio <giovanni.lodi42@gmail.com>
2014-04-09 08:48:17 +0100

    Fixed generator

--- generator.rb
+++ generator.rb
@@ -2,14 +2,14 @@
 colors_data = []
 
 header_template = <<TEMPLATE
-+ (UIColor *)css@name@Color;
++ (UIColor *)css@nameColor;
 
 TEMPLATE
 
 implementation_template = <<TEMPLATE
-+ (UIColor *)css@name@Color
++ (UIColor *)css@nameColor
 {
-    return [UIColor colorWithRed:@red@ green:@green@ blue:@blue@ alpha:1.0];
+    return [UIColor colorWithRed:@red green:@green blue:@blue alpha:1.0];
 }
 
 TEMPLATE
@@ -24,13 +24,17 @@ File.readlines("css_colors.txt").each do |line|
 end
 
 colors_data.each do |color|
-  puts header_template.gsub '@name', color[:name]
+  puts header_template.gsub '@name', color[:name].capitalize
 end
 
 colors_data.each do |color|
   puts implementation_template
-    .gsub( '@name@', color[:name].capitalize )
-    .gsub( '@red@', "#{color[:red].to_f / 255.0}" )
-    .gsub( '@green@', "#{color[:green].to_f / 255.0}" )
-    .gsub( '@blue@', "#{color[:blue].to_f / 255.0}" )
+    .gsub( '@name', color[:name].capitalize )
+    .gsub( '@red', "#{color[:red].to_f / 255.0}" )
+    .gsub( '@green', "#{color[:green].to_f / 255.0}" )
+    .gsub( '@blue', "#{color[:blue].to_f / 255.0}" )
+end
+
+colors_data.each do |color|
+  puts "@\"#{color[:name].capitalize}\","
 end
